eeba5e01f6ce0798aad324a0f4627088ab68917b
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-10-21 16:02:48 +0900

    abstract method implement

--- GameObject/aiControl.py
+++ GameObject/aiControl.py
@@ -5,7 +5,13 @@ class AIControl(Controller):
     def __init__(self, character):
         super().__init__(character)
 
+    def update(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
     def draw(self):
-        self.client.image.clip_draw(self.client.frame * 100, self.client.action * 100,
+        self.client.image.clip_draw(100, 100,
                                     100, 100,
-                                    self.client.x, self.client.y)
+                                    self.client.position.x, self.client.position.y)
