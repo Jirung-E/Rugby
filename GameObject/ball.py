510c7dcb45b8ea46f8da34ce1ab6f5fe9e3e8072
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-10-21 16:58:13 +0900

    ball bounce

--- GameObject/ball.py
+++ GameObject/ball.py
@@ -1,19 +1,43 @@
 from Math import *
 
+import random
+
 from pico2d import Image, load_image
 
 
 class Ball:
     def __init__(self):
         self.position = Point(0, 0)
-        self.direction = Vector(0, 0)
-        self.run_speed = Vector(5, 3)
+        self.height = 5     # position z
+        self.velocity = Vector(0, 0)
+        self.velocity_z = random.uniform(5, 10)
+        self.gravity = 0.1
+        self.rotate = 0
+        self.rotate_power = random.uniform(0.05, 0.3)
+        self.rotate_dir = random.choice([-1, 1])
 
         self.image: Image = load_image('res/ball_small.png')
 
     def update(self):
-        self.position.x += self.direction.x * self.run_speed.x
-        self.position.y += self.direction.y * self.run_speed.y
+        self.position.x += self.velocity.x
+        self.position.y += self.velocity.y
+        
+        self.height += self.velocity_z
+        self.velocity_z -= self.gravity
+        if self.height <= 0:
+            self.height = 0
+
+            if abs(self.velocity_z) < 1:
+                self.velocity_z = 0
+            else:
+                self.velocity_z = -self.velocity_z * 0.7
+
+            self.rotate_power -= 0.002
+            if self.rotate_power < 0:
+                self.rotate_power = 0
+
+        self.rotate += self.rotate_power * self.rotate_dir
+
 
     def draw(self):
-        self.image.draw(self.position.x, self.position.y)
+        self.image.composite_draw(self.rotate, '', self.position.x, self.position.y + self.height)
