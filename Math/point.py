a69d2d058fb02fc19c2754628e1e1a2030326077
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-10-25 17:23:46 +0900

    get distance between two points

--- Math/point.py
+++ Math/point.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -5,3 +8,11 @@ class Point:
 
     def __add__(self, rhs):
         return Point(self.x + rhs.x, self.y + rhs.y)
+    
+    @staticmethod
+    def distance(a, b):
+        return math.sqrt(Point.distance2(a, b))
+    
+    @staticmethod
+    def distance2(a, b):
+        return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
