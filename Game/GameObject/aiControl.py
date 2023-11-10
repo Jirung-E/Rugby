762ce41f38c6c5ddb346d6bc9f8ba62a1731dae1
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-11-10 23:59:49 +0900

    refactoring - aiControl:: add 'run_to_goal' method

--- Game/GameObject/aiControl.py
+++ Game/GameObject/aiControl.py
@@ -9,24 +9,11 @@ class AIControl(Controller):
 
     def update(self):
         if self.client.ball is not None:
+            self.run_to_goal()
             if self.client.position.x > 700:
                 self.client.throw(500, 300)
             elif self.client.position.x < 100:
                 self.client.throw(300, 300)
-                
-            if self.client.stemina > 50:
-                self.client.current_state.dash()
-            elif self.client.stemina > 0:
-                self.client.current_state.run()
-            else:
-                self.client.throw(400, 300)
-
-            if self.client.team == 1:
-                self.client.direction.x = 1
-                self.client.flip = False
-            else:
-                self.client.direction.x = -1
-                self.client.flip = True
         else:
             self.client.current_state.idle()
             self.client.direction.x = 0
@@ -41,4 +28,21 @@ class AIControl(Controller):
                 self.client.flip = False
             elif self.client.direction.x < 0:
                 self.client.flip = True
-                
+
+    def run_to_goal(self):
+        if self.client.stemina > 50:
+            self.client.current_state.dash()
+        elif self.client.stemina > 0:
+            self.client.current_state.run()
+        else:
+            self.client.throw(400, 300)
+
+        if self.client.team == 1:
+            self.client.direction.x = 1
+            self.client.flip = False
+        else:
+            self.client.direction.x = -1
+            self.client.flip = True
+
+    def run_to_ball(self):
+        pass
