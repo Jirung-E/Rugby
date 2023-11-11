208ec9322f816439b91f05c528d1f5464ba8db7f
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-11-12 00:53:18 +0900

    Refactor Controllable class event handling

--- Game/GameObject/controllable.py
+++ Game/GameObject/controllable.py
@@ -1,8 +1,6 @@
 from Game.GameObject.controller import Controller
+import Game.play_scene as play_scene
 import Game.world as world
-from Game.GameObject.ball import Ball
-from Game.GameObject.player import Player
-from Math import Point
 
 from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDLK_w, SDLK_a, SDLK_s, SDLK_d, SDLK_LSHIFT, SDLK_SPACE, \
     SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT, SDL_BUTTON_RIGHT
@@ -13,46 +11,17 @@ class Controllable(Controller):
     def __init__(self, client):
         super().__init__(client)
         self.dash = False
-        self.handling = False
 
     def update(self):
         pass
 
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN:
-            if event.key == SDLK_a:
-                self.client.direction.x -= 1
-            elif event.key == SDLK_d:
-                self.client.direction.x += 1
-            elif event.key == SDLK_w:
-                self.client.direction.y += 1
-            elif event.key == SDLK_s:
-                self.client.direction.y -= 1
-            elif event.key == SDLK_LSHIFT:
-                if self.client.stemina > 0:
-                    self.dash = True
-            elif event.key == SDLK_SPACE:
-                self.handling = True
+            self.handle_keydown(event.key)
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_a:
-                self.client.direction.x += 1
-            elif event.key == SDLK_d:
-                self.client.direction.x -= 1
-            elif event.key == SDLK_w:
-                self.client.direction.y -= 1
-            elif event.key == SDLK_s:
-                self.client.direction.y += 1
-            elif event.key == SDLK_LSHIFT:
-                self.dash = False
-            elif event.key == SDLK_SPACE:
-                self.handling = False
-                self.client.release()
+            self.handle_keyup(event.key)
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            HEIGHT = get_canvas_height()
-            if event.button == SDL_BUTTON_LEFT:
-                self.client.throw(event.x, HEIGHT - event.y)
-            elif event.button == SDL_BUTTON_RIGHT:
-                self.client.tackle(event.x, HEIGHT - event.y)
+            self.handle_mousedown(event.button, event.x, event.y)
 
         if self.client.direction.x == 0 and self.client.direction.y == 0:
             self.client.current_state.idle()
@@ -67,11 +36,54 @@ class Controllable(Controller):
             elif self.client.direction.x < 0:
                 self.client.flip = True
 
+    def handle_keydown(self, key):
+        if key == SDLK_a:
+            self.client.direction.x -= 1
+        elif key == SDLK_d:
+            self.client.direction.x += 1
+        elif key == SDLK_w:
+            self.client.direction.y += 1
+        elif key == SDLK_s:
+            self.client.direction.y -= 1
+        elif key == SDLK_LSHIFT:
+            if self.client.stemina > 0:
+                self.dash = True
+        elif key == SDLK_SPACE:
+            if self.client.ball is not None:
+                return
+            if self.client.grabbed_opponent is not None:
+                return
+            
+            if play_scene.ball.owner is None:
+                if world.collide(self.client, play_scene.ball):
+                    self.client.catch(play_scene.ball)
+                    return
+
+            for other in play_scene.team[self.client.team & 1]:     # 0&2 == 0, 1&1 == 1
+                if world.collide(self.client, other):
+                    self.client.grab(other)
+                    break
+
+    def handle_keyup(self, key):
+        if key == SDLK_a:
+            self.client.direction.x += 1
+        elif key == SDLK_d:
+            self.client.direction.x -= 1
+        elif key == SDLK_w:
+            self.client.direction.y -= 1
+        elif key == SDLK_s:
+            self.client.direction.y += 1
+        elif key == SDLK_LSHIFT:
+            self.dash = False
+        elif key == SDLK_SPACE:
+            self.client.release()
+
+    def handle_mousedown(self, button, x, y):
+        HEIGHT = get_canvas_height()
+        if button == SDL_BUTTON_LEFT:
+            self.client.throw(x, HEIGHT - y)
+        elif button == SDL_BUTTON_RIGHT:
+            self.client.tackle(x, HEIGHT - y)
+
     def handle_collision(self, group, other):
-        if not self.handling:
-            return
-        
-        if group == "ball:player":
-            self.client.catch(other)
-        elif group == "player:player":
-            self.client.grab(other)
+        pass
