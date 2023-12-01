8fd30b45645944a951da78dd784f01da769e9c68
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-12-02 02:45:26 +0900

    lobby scene

---
+++ Game/lobby_scene.py
@@ -0,0 +1,40 @@
+from pico2d import *
+
+import Game.game_framework as game_framework
+import Game.play_scene as play_scene
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_mode(play_scene)
+
+def init():
+    pass
+
+def finish():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
