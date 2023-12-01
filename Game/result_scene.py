96e7540a934146366c08f463280edcdfc4769fdc
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-12-01 14:31:28 +0900

    making result scene

---
+++ Game/result_scene.py
@@ -0,0 +1,42 @@
+import random
+
+from pico2d import *
+
+import Game.game_framework as game_framework
+import Game.world as world
+from Game.GameObject import *
+from Math import *
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
+def init():
+    pass
+
+def finish():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
